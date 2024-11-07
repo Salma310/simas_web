74d5c1b2148692c0d7984d68824b29a180894c54
Salma310 <nasywatwentytwo@gmail.com>
2024-11-07 15:57:37 +0700

    Add API Login

--- app/Http/Controllers/Api/LoginController.php
+++ app/Http/Controllers/Api/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
 
 class LoginController extends Controller
 {
@@ -18,5 +20,35 @@ class LoginController extends Controller
             return response()->json($validator->errors(), 422);
         }
         
+        //get credentials from request
+        $credentials = $request->only('username', 'password');
+
+        //if auth failed
+        if(!$token = auth()->guard('api')->attempt($credentials)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Username atau Password Anda salah'
+            ], 401);
+        }
+
+        //if auth success
+        return response()->json([
+            'success' => true,
+            'user' => auth()->guard('api')->user(),
+            'token' => $token
+        ], 200);
+    }
+
+    public function getUser(Request $request)
+    {
+        // Mendapatkan user yang sedang login
+        $user = Auth::user();
+
+        // Cek jika user ada
+        if (!$user) {
+            return response()->json(['success' => false, 'message' => 'User tidak ditemukan'], 404);
+        }
+
+        return response()->json(['success' => true, 'user' => $user], 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\LoginController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::get('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::middleware('auth:sanctum')->get('/user', [LoginController::class, 'getUser'])->name('user');
