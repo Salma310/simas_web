af9c30316c4050bb9dd2942822cb8d8ffcbafb36
Salma310 <nasywatwentytwo@gmail.com>
2024-11-12 13:54:47 +0700

    add route api

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\LoginController;
-
+use App\Http\Controllers\Api\RoleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,4 +22,22 @@ use App\Http\Controllers\Api\LoginController;
 
 Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
 Route::get('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
-Route::middleware('auth:sanctum')->get('/user', [LoginController::class, 'getUser'])->name('user');
+// Route::middleware('auth:sanctum')->get('/user', [LoginController::class, 'getUser'])->name('user');
+
+Route::get('roles', [RoleController::class, 'index']);
+Route::post('roles', [RoleController::class, 'store']);
+Route::get('roles/{role}', [RoleController::class, 'show']);
+Route::put('roles/{role}', [RoleController::class, 'update']);
+Route::delete('roles/{role}', [RoleController::class, 'destroy']);
+
+Route::get('users', [App\Http\Controllers\Api\UserController::class, 'index']);
+Route::post('users', [App\Http\Controllers\Api\UserController::class, 'store']);
+Route::get('users/{user}', [App\Http\Controllers\Api\UserController::class, 'show']);
+Route::put('users/{user}', [App\Http\Controllers\Api\UserController::class, 'update']);
+Route::delete('users/{user}', [App\Http\Controllers\Api\UserController::class, 'destroy']);
+
+Route::get('events', [App\Http\Controllers\Api\EventController::class, 'index']);
+Route::post('events', [App\Http\Controllers\Api\EventController::class, 'store']);
+Route::get('events/{event}', [App\Http\Controllers\Api\EventController::class, 'show']);
+Route::put('events/{event}', [App\Http\Controllers\Api\EventController::class, 'update']);
+Route::delete('events/{event}', [App\Http\Controllers\Api\EventController::class, 'destroy']);
